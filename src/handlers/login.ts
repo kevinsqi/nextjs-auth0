d28baf9c9b1267c524ebf00458b1f7c00485b9b8
Kevin Qi <kevin@gridmatic.com>
2023-01-20 02:06:32 +0000

    TEMP add debug logging to handleLogin

--- src/handlers/login.ts
+++ src/handlers/login.ts
@@ -269,6 +269,7 @@ export default function handleLoginFactory(
     res?: NextApiResponse,
     options?: LoginOptions
   ): any => {
+    console.log('DEBUG OPTIONS', options);
     if (reqOrOptions instanceof IncomingMessage && res) {
       return login(reqOrOptions, res, options);
     }
